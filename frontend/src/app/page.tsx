91d6bb583e406e68597d359c59ecb14838e52389
Lucas Matos Oliveira <lucasmatos.bis@gmail.com>
2024-08-22 13:45:30 -0300

    Build error fixed

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,9 +2,14 @@
 
 import { useRouter } from "next/navigation";
 import { AppRoutes } from "@/commom/http/app-routes";
+import { useEffect } from "react";
 
 export default function Home() {
     const router = useRouter();
 
-    router.push(AppRoutes.Login());
+    useEffect(() => {
+        router.push(AppRoutes.Login());
+    }, [router]);
+
+    return null;
 }
